e04c07720e9988854304cc19a69d3fffb357bbcd
Jagruti <jagrutirkm@gmail.com>
2022-07-05 01:06:34 +0530

    profile update

--- register/views.py
+++ register/views.py
@@ -14,8 +14,8 @@ from django.shortcuts import HttpResponseRedirect
 
 
 def employee_list(request):
-    context = {'employee_list': Employee.objects.all()}
-    return render(request, "register/employee_list.html", context)
+    context = {'adminprofile': Employee.objects.all()}
+    return render(request, "admin_profile.html", context)
 
 
 def employee_form(request, id=0):
@@ -46,24 +46,6 @@ def employee_delete(request, id):
 def home(request):
     return render(request, 'home/login.html')
 
-# def employee_form(request):
-#     if request.user.is_authenticated:
-#         return redirect('/')
-#     if request.method == 'POST':
-#         form = EmployeeForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             username = form.cleaned_data.get('username')
-#             password = form.cleaned_data.get('password1')
-#             user = authenticate(username=username, password=password)
-#             login(request, user)
-#             return redirect('../profile/')
-#         else:
-#             return render(request, 'register/employee_form.html', {'form': form})
-#     else:
-#         form = EmployeeForm()
-#         return render(request, 'register/employee_form.html', {'form': form})
-
 
 def signin(request):
 
@@ -72,19 +54,21 @@ def signin(request):
         username = request.POST['username']
         password = request.POST['password']
         # print("myoutput", username+password)
+        # check if user is already registered
         isLoginTrue = 0
         user_data = 0
-
-        for ldata in login_data:
-            if (ldata.username == username) and (ldata.password == password):
+        for i in login_data:
+            if (i.username == username) and (i.password == password):
                 isLoginTrue = 1
-                user_data = ldata
-        if (isLoginTrue == 1) and (user_data != 0) and (user_data.user_type_id == 1):
+                user_data = i  # user_data is the user object
+
+        if (isLoginTrue == 1) and (user_data.user_type_id == 1):
             return render(request, "profile.html", {'ldata': login_data})
-        elif (isLoginTrue == 1) and (user_data != 0) and (user_data.user_type_id == 2):
+        elif (isLoginTrue == 1) and (user_data.user_type_id == 2):
             return render(request, "admin_profile.html", {'ldata': login_data})
         else:
-            return HttpResponse('user detail wrong')
+            # show error message on same html page
+            return render(request, "home/login.html", {'error': 'Invalid username or password'})
 
 
 def profile(request):
